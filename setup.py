aaa86e06ea43b17b371daa135601647fac3ee9ff
Scott Zarecor <scott@zarecor.com>
2021-01-13 14:37:09 -0600

    fixing name

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="CAFA Swissprot Targets",
+    name="cafa-swissprot",
+    #name="CAFA Swissprot Targets",
     version="0.0.1",
     author="Iddo Friedberg, Scott Zarecor",
     author_email=None,
